639cda5f2cd99a94ff3cf917cef8de928d164c4a
lembolov9 <Tomas080808>
2019-04-09 15:05:52 +0300

    change dataset

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ def train_net(net,
               epochs=5,
               batch_size=1,
               lr=0.1,
-              val_percent=0.05,
+              val_percent=0.25,
               save_cp=True,
               gpu=False,
               img_scale=0.5):
