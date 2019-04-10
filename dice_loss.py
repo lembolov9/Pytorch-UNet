925aa668cf2822bf876ea5b1772bbdd1ca40c440
lembolov9 <Tomas080808>
2019-04-10 19:56:04 +0300

    fd

--- dice_loss.py
+++ dice_loss.py
@@ -5,13 +5,18 @@ class DiceCoeff(Function):
     """Dice coeff for individual examples"""
 
     def forward(self, input, target):
-        self.save_for_backward(input, target)
-        eps = 0.000000001
-        self.inter = torch.dot(input.contiguous().view( -1), target.contiguous().view( -1))
-        self.union = torch.sum(input) + torch.sum(target) + eps
 
-        t = (2 * self.inter.float() + eps) / self.union.float()
-        return t
+        smooth = 1.
+
+        # have to use contiguous since they may from a torch.view op
+        iflat = input.contiguous().view(-1)
+        tflat = target.contiguous().view(-1)
+        intersection = (iflat * tflat).sum()
+
+        A_sum = torch.sum(tflat * iflat)
+        B_sum = torch.sum(tflat * tflat)
+
+        return 1 - ((2. * intersection + smooth) / (A_sum + B_sum + smooth))
 
     # This function has only a single output, so it gets only one gradient
     def backward(self, grad_output):
@@ -26,24 +31,7 @@ class DiceCoeff(Function):
             grad_target = None
 
         return grad_input, grad_target
-def dice_loss(pred, target):
-    """This definition generalize to real valued pred and target vector.
-This should be differentiable.
-    pred: tensor with first dimension as batch
-    target: tensor with first dimension as batch
-    """
 
-    smooth = 1.
-
-    # have to use contiguous since they may from a torch.view op
-    iflat = pred.contiguous().view(-1)
-    tflat = target.contiguous().view(-1)
-    intersection = (iflat * tflat).sum()
-
-    A_sum = torch.sum(tflat * iflat)
-    B_sum = torch.sum(tflat * tflat)
-
-    return 1 - ((2. * intersection + smooth) / (A_sum + B_sum + smooth) )
 
 def dice_coeff(input, target):
     """Dice coeff for batches"""
@@ -53,6 +41,6 @@ def dice_coeff(input, target):
         s = torch.FloatTensor(1).zero_()
 
     for i, c in enumerate(zip(input, target)):
-        s += dice_loss(c[0],c[1])
+        s = s + DiceCoeff().forward(c[0], c[1])
 
-    return s / (i+1)
+    return s / (i + 1)
